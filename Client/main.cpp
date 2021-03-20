a90764facf57c551048615b137fa12739a1e52b2
Sam Collier <samcollier916@gmail.com>
2021-03-20 09:57:36 -0400

    Improve client UI

--- Client/main.cpp
+++ Client/main.cpp
@@ -4,8 +4,8 @@
 
 int _cdecl main(int argc, char** argv)
 {
+	std::cout << "Type 'exit' to leave the server" << std::endl;
 	Client client(argv[1]);
-	std::cout << argv[1];
 	std::string input;
 	while(input!="exit")
 	{
